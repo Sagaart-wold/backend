f4e6fdab6185433ea541e619936a095b92da1239
Leonid <workemail.leonid@gmail.com>
2024-06-22 19:11:41 +0300

    Описана модель USER и ADMIN

--- sagaart_backend/users/admin.py
+++ sagaart_backend/users/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'created_at')
+    search_fields = ('email', 'first_name', 'last_name')
+    readonly_fields = ('created_at',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'address')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'roles')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'is_active', 'is_staff', 'roles'),
+        }),
+    )
+
+    ordering = ('email',)
+
+    # Убираем list_filter и filter_horizontal, так как они используют поля, которые не определены в модели User
+    list_filter = ()
+    filter_horizontal = ()
+
+admin.site.register(User, CustomUserAdmin)
+
+
+
+
+
+
+
+
+
+
--- sagaart_backend/users/models.py
+++ sagaart_backend/users/models.py
@@ -3,81 +3,36 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, user_id, first_name, last_name, phone_number, password=None, subscription_id=None, order_id=None, favourite_artist_id=None, favourite_art_obj_id=None):
-        if not user_id:
-            raise ValueError('У пользователей должен быть user_id')
-        user = self.model(
-            user_id=user_id,
-            user_first_name=first_name,
-            user_last_name=last_name,
-            user_phone_number=phone_number,
-            subscription_id=subscription_id,
-            order_id=order_id,
-            favourite_artist_id=favourite_artist_id,
-            favourite_art_obj_id=favourite_art_obj_id
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, user_id, first_name, last_name, phone_number, password=None, subscription_id=None, order_id=None, favourite_artist_id=None, favourite_art_obj_id=None):
-        user = self.create_user(
-            user_id=user_id,
-            first_name=first_name,
-            last_name=last_name,
-            phone_number=phone_number,
-            password=password,
-            subscription_id=subscription_id,
-            order_id=order_id,
-            favourite_artist_id=favourite_artist_id,
-            favourite_art_obj_id=favourite_art_obj_id
-        )
-        user.is_superuser = True
-        user.is_staff = True
+    def create_user(self, email, password=None, first_name='', last_name='', **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, **extra_fields)
+        if password:
+            user.set_password(password)
         user.save(using=self._db)
         return user
 
 
 class User(AbstractBaseUser):
-    user_id = models.IntegerField(primary_key=True)
-    user_first_name = models.CharField(max_length=30)
-    user_last_name = models.CharField(max_length=30)
-    user_phone_number = models.IntegerField()
-    password = models.CharField(max_length=128)
-    subscription_id = models.ForeignKey('Subscription', on_delete=models.CASCADE)
-    order_id = models.ForeignKey('Order', on_delete=models.SET_NULL)
-    favourite_artist_id = models.ForeignKey('Artist', on_delete=models.SET_NULL)
-    favourite_art_obj_id = models.ForeignKey('ArtObject', on_delete=models.SET_NULL)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # Поле пароля
+    roles = models.CharField(max_length=255, blank=True, null=True)  # Роли пользователя
+    created_at = models.DateTimeField(auto_now_add=True, null=True)  # Дата создания аккаунта
+    first_name = models.CharField(max_length=30)  # Имя пользователя
+    last_name = models.CharField(max_length=30)  # Фамилия пользователя
+    phone = models.CharField(max_length=15, blank=True, null=True)  # Номер телефона
+    address = models.TextField(blank=True)  # Адрес пользователя
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'user_id'
-    REQUIRED_FIELDS = ['user_first_name', 'user_last_name', 'user_phone_number', 'subscription_id', 'order_id', 'favourite_artist_id', 'favourite_art_obj_id']
-
-    def get_username(self):
-        return str(self.user_id)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def get_full_name(self):
-        return f"{self.user_first_name} {self.user_last_name}"
+        return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
-        return self.get_full_name()
+        return self.email
 
-
-class Subscription(models.Model):
-    subscription_id =
-
-
-class Order(models.Model):
-    order_id = 
-
-
-class Artist(models.Model):
-    artist_id = 
-
-
-class ArtObject(models.Model):
-    art_obj_id = 

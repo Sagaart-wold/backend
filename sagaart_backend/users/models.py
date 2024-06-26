86e35af575308ac2c94795252e14773ed276e5ab
Leonid <workemail.leonid@gmail.com>
2024-06-26 12:14:41 +0300

    fix

--- sagaart_backend/users/models.py
+++ sagaart_backend/users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from enum import Enum
 
-# Create your models here.
+
+class UserRole(Enum):
+    ADMIN = 'admin'
+    USER = 'user'
+    SELLER = 'seller'
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    role = models.CharField(
+        choices=[(role.value, role.name) for role in UserRole],
+        default=UserRole.USER.value,
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'

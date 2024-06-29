2ed405a07bc2a1dd25df4e71725f90cbddc3c1e2
Badmajor <v.balonkin@gmail.com>
2024-06-29 11:45:06 +0300

    optimized sql queries

--- sagaart_backend/api/v1/artobjects/views.py
+++ sagaart_backend/api/v1/artobjects/views.py
@@ -11,5 +11,5 @@ class ArtistViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, GenericVie
 
     def get_queryset(self):
         return Artist.objects.select_related(
-            "city_of_birth", "city_of_living", "photo"
+            "city_of_birth", "city_of_living", "photo", 'city_of_birth__country', 'city_of_living__country'
         ).prefetch_related("favorited_by")

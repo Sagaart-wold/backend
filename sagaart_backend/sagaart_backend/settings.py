4ea9f3fd42c756b06417c79d4e95b2233a0eae49
Badmajor <v.balonkin@gmail.com>
2024-07-02 20:36:59 +0300

    fix

--- sagaart_backend/sagaart_backend/settings.py
+++ sagaart_backend/sagaart_backend/settings.py
@@ -88,7 +88,6 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
 
